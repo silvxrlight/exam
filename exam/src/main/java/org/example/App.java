718e599b718a176075aea53988482880ea593f46
Gleb Popov <72411566+silvxrlight@users.noreply.github.com>
2025-08-29 14:22:53 +0300

    Delete exam directory

--- exam/src/main/java/org/example/App.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import javax.swing.*;
-
-public class App {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new UserUI().setVisible(true));
-    }
-}
